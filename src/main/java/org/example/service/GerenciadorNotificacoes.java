68e94b951b6f20bb1fd210295d36e343c39442a5
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-07-22 20:22:38 -0300

    feat: transforma a classe em service e faz a injeção da dependencia CanalNotificacao

--- src/main/java/org/example/service/GerenciadorNotificacoes.java
+++ src/main/java/org/example/service/GerenciadorNotificacoes.java
@@ -4,17 +4,26 @@ import org.example.config.exception.ReciboNaoEncontradoException;
 import org.example.entity.CanalNotificacao;
 import org.example.entity.FiltroAuditoria;
 import org.example.entity.ReciboImutavel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
+@Service
 public class GerenciadorNotificacoes {
 
-    public List<ReciboImutavel> disparar(String destinatario, String mensagem, List<CanalNotificacao> canais) {
+    private final List<CanalNotificacao> canais;
+
+    public GerenciadorNotificacoes(List<CanalNotificacao> canais) {
+        this.canais = canais;
+    }
+
+    public List<ReciboImutavel> disparar(String destinatario, String mensagem) {
         List<ReciboImutavel> recibos = new ArrayList<>();
         int idTransacao = 1;
 
-        for(CanalNotificacao canal : canais){
+        for(CanalNotificacao canal : this.canais){
             boolean sucesso = canal.enviar(destinatario, mensagem);
             String nomeDoCanal = canal.getClass().getSimpleName();
             ReciboImutavel recibo = new ReciboImutavel(idTransacao, destinatario, nomeDoCanal, sucesso, LocalDateTime.now());
@@ -29,9 +38,9 @@ public class GerenciadorNotificacoes {
         Set<Integer> idsVistos = new HashSet<>();
 
         for(ReciboImutavel recibo : recibos) {
-            if(!idsVistos.contains(recibo.id())) {
+            if(!idsVistos.contains(recibo.getId())) {
                 recibosVerificados.add(recibo);
-                idsVistos.add(recibo.id());
+                idsVistos.add(recibo.getId());
             }
         }
 
@@ -43,7 +52,7 @@ public class GerenciadorNotificacoes {
         Set<ReciboImutavel> recibosSucesso = new HashSet<>();
         Set<ReciboImutavel> recibosFalha = new HashSet<>();
         for(ReciboImutavel recibo : recibos) {
-            if(recibo.sucesso()){
+            if(recibo.getSucesso()){
                 recibosSucesso.add(recibo);
             } else {
                 recibosFalha.add(recibo);
@@ -57,13 +66,13 @@ public class GerenciadorNotificacoes {
     public ReciboImutavel buscarRecibo(Integer idTransacao, List<ReciboImutavel> recibos) {
         Map<Integer, ReciboImutavel> recibosMap = new HashMap<>();
         for(ReciboImutavel recibo : recibos){
-            recibosMap.putIfAbsent(recibo.id(), recibo);
+            recibosMap.putIfAbsent(recibo.getId(), recibo);
         }
         return Optional.ofNullable(recibosMap.get(idTransacao)).orElseThrow(() -> new ReciboNaoEncontradoException("Recibo não encontrado para o id: " + idTransacao));
     }
 
     public List<String> obterEmailsSucesso(List<ReciboImutavel> recibos) {
-        return recibos.stream().filter(recibo -> recibo.sucesso()).filter(recibo -> recibo.canal().contains("NotificacaoEmail")).map(recibo -> recibo.destinatario()).toList();
+        return recibos.stream().filter(recibo -> recibo.getSucesso()).filter(recibo -> recibo.getCanal().contains("NotificacaoEmail")).map(recibo -> recibo.getDestinatario()).toList();
     }
 
     public Map<String, Double> calcularCusto(List<ReciboImutavel> recibos) {
@@ -74,9 +83,9 @@ public class GerenciadorNotificacoes {
         ));
 
         for(ReciboImutavel recibo : recibos) {
-            if(recibo.sucesso())
+            if(recibo.getSucesso())
             {
-                switch (recibo.canal()) {
+                switch (recibo.getCanal()) {
                     case "NotificacaoEmail" -> custos.put("Email", custos.get("Email") + 0.01);
                     case "NotificacaoSms" -> custos.put("Sms", custos.get("Sms") + 0.05);
                     case "NotificacaoPush" -> custos.put("Push", custos.get("Push") + 0.02);
@@ -87,11 +96,11 @@ public class GerenciadorNotificacoes {
     }
 
     public List<String> obterContatosUnicos(List<ReciboImutavel> recibos) {
-        return recibos.stream().flatMap(recibo -> Arrays.stream(recibo.destinatario().split(","))).map(String::trim).distinct().toList();
+        return recibos.stream().flatMap(recibo -> Arrays.stream(recibo.getDestinatario().split(","))).map(String::trim).distinct().toList();
     }
 
     public List<ReciboImutavel> ordenarLista(List<ReciboImutavel> recibos) {
-        return recibos.stream().sorted(Comparator.comparing(ReciboImutavel::dataHora).reversed()).toList();
+        return recibos.stream().sorted(Comparator.comparing(ReciboImutavel::getDataHora).reversed()).toList();
     }
 
     public List<ReciboImutavel> filtrarLista(List<ReciboImutavel> recibos, FiltroAuditoria filtro) {
