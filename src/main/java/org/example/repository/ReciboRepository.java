2a7173048ea2d10fce461c0a3679290f52260f73
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-07-22 23:22:33 -0300

    feat: adiciona queries personalizadas

--- src/main/java/org/example/repository/ReciboRepository.java
+++ src/main/java/org/example/repository/ReciboRepository.java
@@ -4,8 +4,19 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import org.example.entity.ReciboImutavel;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReciboRepository extends JpaRepository<ReciboImutavel, Integer> {
+
+    List<ReciboImutavel> findAllBySucesso(Boolean sucesso);
+
+    List<ReciboImutavel> findAllByCanalIgnoreCase(String canal);
+
+    @Query("SELECT r FROM ReciboImutavel r WHERE r.destinatario LIKE %:dominio")
+    List<ReciboImutavel> findAllByDominio(String dominio);
+
 }
